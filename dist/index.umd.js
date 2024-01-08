695ce9a00839b15073e76b557a30e627b68ab50f
Cheng Jianhua <w.chengjianhua@gmail.com>
2024-01-08 12:00:41 +0800

    feat: push dist to remote

---
+++ dist/index.umd.js
@@ -0,0 +1,167 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.howLongUntilLunch = {}));
+})(this, (function (exports) { 'use strict';
+
+	const copyProperty = (to, from, property, ignoreNonConfigurable) => {
+		// `Function#length` should reflect the parameters of `to` not `from` since we keep its body.
+		// `Function#prototype` is non-writable and non-configurable so can never be modified.
+		if (property === 'length' || property === 'prototype') {
+			return;
+		}
+
+		// `Function#arguments` and `Function#caller` should not be copied. They were reported to be present in `Reflect.ownKeys` for some devices in React Native (#41), so we explicitly ignore them here.
+		if (property === 'arguments' || property === 'caller') {
+			return;
+		}
+
+		const toDescriptor = Object.getOwnPropertyDescriptor(to, property);
+		const fromDescriptor = Object.getOwnPropertyDescriptor(from, property);
+
+		if (!canCopyProperty(toDescriptor, fromDescriptor) && ignoreNonConfigurable) {
+			return;
+		}
+
+		Object.defineProperty(to, property, fromDescriptor);
+	};
+
+	// `Object.defineProperty()` throws if the property exists, is not configurable and either:
+	// - one its descriptors is changed
+	// - it is non-writable and its value is changed
+	const canCopyProperty = function (toDescriptor, fromDescriptor) {
+		return toDescriptor === undefined || toDescriptor.configurable || (
+			toDescriptor.writable === fromDescriptor.writable
+			&& toDescriptor.enumerable === fromDescriptor.enumerable
+			&& toDescriptor.configurable === fromDescriptor.configurable
+			&& (toDescriptor.writable || toDescriptor.value === fromDescriptor.value)
+		);
+	};
+
+	const changePrototype = (to, from) => {
+		const fromPrototype = Object.getPrototypeOf(from);
+		if (fromPrototype === Object.getPrototypeOf(to)) {
+			return;
+		}
+
+		Object.setPrototypeOf(to, fromPrototype);
+	};
+
+	const wrappedToString = (withName, fromBody) => `/* Wrapped ${withName}*/\n${fromBody}`;
+
+	const toStringDescriptor = Object.getOwnPropertyDescriptor(Function.prototype, 'toString');
+	const toStringName = Object.getOwnPropertyDescriptor(Function.prototype.toString, 'name');
+
+	// We call `from.toString()` early (not lazily) to ensure `from` can be garbage collected.
+	// We use `bind()` instead of a closure for the same reason.
+	// Calling `from.toString()` early also allows caching it in case `to.toString()` is called several times.
+	const changeToString = (to, from, name) => {
+		const withName = name === '' ? '' : `with ${name.trim()}() `;
+		const newToString = wrappedToString.bind(null, withName, from.toString());
+		// Ensure `to.toString.toString` is non-enumerable and has the same `same`
+		Object.defineProperty(newToString, 'name', toStringName);
+		Object.defineProperty(to, 'toString', {...toStringDescriptor, value: newToString});
+	};
+
+	function mimicFunction(to, from, {ignoreNonConfigurable = false} = {}) {
+		const {name} = to;
+
+		for (const property of Reflect.ownKeys(from)) {
+			copyProperty(to, from, property, ignoreNonConfigurable);
+		}
+
+		changePrototype(to, from);
+		changeToString(to, from, name);
+
+		return to;
+	}
+
+	const calledFunctions = new WeakMap();
+
+	const onetime = (function_, options = {}) => {
+		if (typeof function_ !== 'function') {
+			throw new TypeError('Expected a function');
+		}
+
+		let returnValue;
+		let callCount = 0;
+		const functionName = function_.displayName || function_.name || '<anonymous>';
+
+		const onetime = function (...arguments_) {
+			calledFunctions.set(onetime, ++callCount);
+
+			if (callCount === 1) {
+				returnValue = function_.apply(this, arguments_);
+				function_ = undefined;
+			} else if (options.throw === true) {
+				throw new Error(`Function \`${functionName}\` can only be called once`);
+			}
+
+			return returnValue;
+		};
+
+		mimicFunction(onetime, function_);
+		calledFunctions.set(onetime, callCount);
+
+		return onetime;
+	};
+
+	onetime.callCount = function_ => {
+		if (!calledFunctions.has(function_)) {
+			throw new Error(`The given function \`${function_.name}\` is not wrapped by the \`onetime\` package`);
+		}
+
+		return calledFunctions.get(function_);
+	};
+
+	const ANIMATION_NAME = "rgh-selector-observer";
+	const getListener = (seenMark, selector, callback, signal) => (event) => {
+	  const target = event.target;
+	  // The target can match a selector even if the animation actually happened on a ::before pseudo-element, so it needs an explicit exclusion here
+	  if (target.classList.contains(seenMark) || !target.matches(selector)) {
+	    return;
+	  }
+
+	  // Removes this specific selector’s animation once it was seen
+	  target.classList.add(seenMark);
+
+	  callback(target, { signal });
+	};
+
+	const registerAnimation = onetime(() => {
+	  const s = document.createElement("style");
+	  s.textContent = `@keyframes ${ANIMATION_NAME} {}`;
+	  document.head.append(s);
+	});
+
+	function observe(selectors, listener, { signal, callerId }) {
+	  if (signal?.aborted) {
+	    return;
+	  }
+
+	  const selector = String(selectors); // Array#toString() creates a comma-separated string
+	  const seenMark = "rgh-seen-" + callerId;
+
+	  registerAnimation();
+
+	  const rule = document.createElement("style");
+
+	  rule.textContent = `
+        :where(${String(selector)}):not(.${seenMark}) {
+          animation: 1ms ${ANIMATION_NAME};
+        }
+      `;
+	  document.body.prepend(rule);
+	  signal?.addEventListener("abort", () => {
+	    rule.remove();
+	  });
+	  window.addEventListener(
+	    "animationstart",
+	    getListener(seenMark, selector, listener, signal),
+	    { signal }
+	  );
+	}
+
+	exports.observe = observe;
+
+}));
